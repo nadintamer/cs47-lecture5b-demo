8f7a45dee9f0eee4d96cd6c85e87eaa3352625c3
Nadin Tamer <nadin.tamer@gmail.com>
2022-10-27 19:38:58 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
+import { NavigationContainer } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+
+import HomeStack from './components/HomeStack';
 
 export default function App() {
-  return (
+  const Tab = createBottomTabNavigator();
+
+  const PinnedTab = () => (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Pinned!</Text>
     </View>
+  );
+
+  const ProfileTab = () => (
+    <View style={styles.container}>
+      <Text>Profile!</Text>
+    </View>
+  );
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'safari'
+            } else if (route.name === 'Pinned') {
+              iconName = 'map-pin'
+            } else if (route.name === 'Profile') {
+              iconName = 'user'
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+        })}>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Pinned" component={PinnedTab} />
+        <Tab.Screen name="Profile" component={ProfileTab} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
